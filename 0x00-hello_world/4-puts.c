6cf4c864b0d15cd1d35be9e6d834b0671b1c3113
DanielProgramming56 <chideberedanieldevelopement@gmail.com>
2022-12-08 23:39:28 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry
- * return - Always 0
+ * Return: Always 0
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
