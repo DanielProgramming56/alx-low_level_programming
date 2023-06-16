8c9039b5ecf369d816aed3a09f95476ec3c3cc6f
DanielProgramming56 <chideberedanieldevelopement@gmail.com>
2023-06-16 17:03:19 +0100

    message

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
-/*
- * print_dlistint - return the number of element
+/**
+ * dlistint_len - return the number of element
  * @h: the header pointer to a linked lists
- * return: element of items
+ * Return: element of items
 */
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while(h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
 
-    return count;
+return (count);
 }
