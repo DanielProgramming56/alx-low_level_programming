4bbc88952f6927a1575518e9649e688bb0e41d8a
DanielProgramming56 <chideberedanieldevelopement@gmail.com>
2023-06-16 17:01:01 +0100

    message

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
-/*
+/**
  * print_dlistint - print all element is a double linked lists
  * @h: the header pointer to a linked lists
- * return: element of items
+ * Description: return the total element  a linkedlist
+ * Return: element of items
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while(h != NULL)
-    {
-        printf("%i\n", h->n);
-        count++;
-        h = h->next;
-    }
+while (h != NULL)
+{
+printf("%i\n", h->n);
+count++;
+h = h->next;
+}
 
-    return count;
+return (count);
 }
